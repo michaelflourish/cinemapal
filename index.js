00570b7c6ceee2c4f518eea129a96d9852d2b0d3
michaelflourish <michaelflourish@hotmail.com>
2023-04-18 22:06:58 -0400

    Latest Commit

---
+++ index.js
@@ -0,0 +1,87 @@
+const http =require('http');
+const path = require('path');
+const fs = require('fs');
+const cors = require('cors');
+
+const server = http.createServer((req, res)=>{
+     
+    if( req.url ==='/'){
+     
+        fs.readFile(path.join(__dirname,'public','index.html'),
+        (err, content)=>{
+
+            if (err) throw err;
+            res.writeHead(200,{ 'Content-type': 'text/html'});
+            res.end(content);
+
+        });
+
+
+    }
+    else if(req.url ==='/images/1.png'){
+
+       
+        fs.readFile(path.join(__dirname,'public/images','1.png'),
+        (err, content)=>{
+            if(err ) throw err;
+            res.writeHead(200, { 'Content-type': 'image/png'})
+            res.end(content)
+        });
+
+
+
+    }
+    else if(req.url ==='/style.css'){
+
+       
+      fs.readFile(path.join(__dirname,'public','style.css'),
+      (err, content)=>{
+          if(err ) throw err;
+          res.writeHead(200, { 'Content-type': 'text/css'})
+          res.end(content)
+      });
+
+
+
+    }
+    else if(req.url ==='/images/2.png'){
+
+       
+      fs.readFile(path.join(__dirname,'public/images','2.png'),
+      (err, content)=>{
+          if(err ) throw err;
+          res.writeHead(200, { 'Content-type': 'image/png'})
+          res.end(content)
+      });
+
+  }
+  else if(req.url ==='/images/mike_flourish.jpg'){
+
+       
+    fs.readFile(path.join(__dirname,'public/images','mike_flourish.jpg'),
+    (err, content)=>{
+        if(err ) throw err;
+        res.writeHead(200, { 'Content-type': 'image/png'})
+        res.end(content)
+    });
+
+  }
+    else if(req.url ==='/api'){
+
+        fs.readFile(path.join(__dirname,'public','db.json'), 'utf-8',
+        (err, content)=>{
+
+            if(err ) throw err ;
+            res.writeHead(200, { 'Content-type': 'application/json'})  
+            res.end(content);
+        });
+
+
+    }
+    else{
+        res.writeHead(404, { 'Content-type': 'text/html'})  
+        res.end("<h1> 404 Nothing is Here </h1>")
+    }
+});
+
+server.listen(5959, ()=> console.log(" great our server is runnning"));
