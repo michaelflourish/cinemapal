5c44ba07f5c2cd5d2cac3e3742d4d8567abde796
michaelflourish <michaelflourish@hotmail.com>
2023-04-20 12:37:58 -0400

    Latest Update for index

--- index.js
+++ index.js
@@ -87,6 +87,7 @@ const server = http.createServer((req, res)=>{
     }
 }); 
 
-server.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+server.listen(process.env.PORT || 3000, () => {
+    console.log('Server is listening on port ' + (process.env.PORT || 3000));
 });
+  
