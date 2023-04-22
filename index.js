506b8d40d97ae2ce8017ce394533583a3197e6cf
michaelflourish <michaelflourish@hotmail.com>
2023-04-22 18:57:09 -0400

    Updated

--- index.js
+++ index.js
@@ -3,78 +3,76 @@ const path = require('path');
 const fs = require('fs');
 const cors = require('cors');
 
-const server = http.createServer((req, res)=>{
-     
-    if( req.url ==='/'){
-     
-        fs.readFile(path.join(__dirname,'public','index.html'),
-        (err, content)=>{
+const server = http.createServer((request, response) => {
+  const { method, url } = request;
 
-            if (err) throw err;
-            res.writeHead(200,{ 'Content-type': 'text/html'});
-            res.end(content);
-
-        });
-    } else if(req.url ==='/api'){
-
-      fs.readFile(path.join(__dirname,'public','db.json'), 'utf-8',
-      (err, content) => {
-          if(err) throw err;
-          res.setHeader("Access-Control-Allow-Origin", "*");
-          res.writeHead(200, { 'Content-type': 'application/json'});
-          res.end(content);
-      });
-  }
-    else if(req.url ==='/style.css'){
-      //styles
-      fs.readFile(path.join(__dirname,'public','style.css'),
-      (err, content)=>{
-          if(err ) throw err;
-          res.writeHead(200, { 'Content-type': 'text/css'});
-          res.end(content);
-      });
-
-
-
-    }
-    else if(req.url ==='/images/2.png'){
-
-       
-      fs.readFile(path.join(__dirname,'public/images','2.png'),
-      (err, content)=>{
-          if(err ) throw err;
-          res.writeHead(200, { 'Content-type': 'image/png'})
-          res.end(content)
-      });
-
-  }
-  else if(req.url ==='/images/mike_flourish.jpg'){
-
-       
-    fs.readFile(path.join(__dirname,'public/images','mike_flourish.jpg'),
-    (err, content)=>{
-        if(err ) throw err;
-        res.writeHead(200, { 'Content-type': 'image/jpg'})
-        res.end(content)
+  if (url === '/') {
+    fs.readFile(path.join(__dirname, 'public', 'index.html'), 'utf-8', (error, content) => {
+      if (error) {
+        response.writeHead(500, { 'Content-Type': 'text/plain' });
+        response.end(`An error occurred: ${error.message}`);
+      } else {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end(content);
+      }
     });
-
-  } else if(req.url ==='/images/1.png'){
-
-       
-    fs.readFile(path.join(__dirname,'public/images','1.png'),
-    (err, content)=>{
-        if(err ) throw err;
-        res.writeHead(200, { 'Content-type': 'image/png'});
-        res.end(content);
+  } else if (url === '/api') {
+    fs.readFile(path.join(__dirname, 'public', 'db.json'), 'utf-8', (error, content) => {
+      if (error) {
+        response.writeHead(500, { 'Content-Type': 'text/plain' });
+        response.end(`An error occurred: ${error.message}`);
+      } else {
+        response.setHeader('Access-Control-Allow-Origin', '*');
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(content);
+      }
     });
-  } else{
-        res.writeHead(404, { 'Content-type': 'text/html'})  
-        res.end("<h1> 404 Nothing is Here </h1>")
-    }
-}); 
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  } else if (url === '/style.css') {
+    fs.readFile(path.join(__dirname, 'public', 'style.css'), 'utf-8', (error, content) => {
+      if (error) {
+        response.writeHead(500, { 'Content-Type': 'text/plain' });
+        response.end(`An error occurred: ${error.message}`);
+      } else {
+        response.writeHead(200, { 'Content-Type': 'text/css' });
+        response.end(content);
+      }
+    });
+  } else if (url === '/images/2.png') {
+    fs.readFile(path.join(__dirname, 'public', 'images', '2.png'), (error, content) => {
+      if (error) {
+        response.writeHead(500, { 'Content-Type': 'text/plain' });
+        response.end(`An error occurred: ${error.message}`);
+      } else {
+        response.writeHead(200, { 'Content-Type': 'image/png' });
+        response.end(content);
+      }
+    });
+  } else if (url === '/images/mike_flourish.jpg') {
+    fs.readFile(path.join(__dirname, 'public', 'images', 'mike_flourish.jpg'), (error, content) => {
+      if (error) {
+        response.writeHead(500, { 'Content-Type': 'text/plain' });
+        response.end(`An error occurred: ${error.message}`);
+      } else {
+        response.writeHead(200, { 'Content-Type': 'image/jpeg' });
+        response.end(content);
+      }
+    });
+  } else if (url === '/images/1.png') {
+    fs.readFile(path.join(__dirname, 'public', 'images', '1.png'), (error, content) => {
+      if (error) {
+        response.writeHead(500, { 'Content-Type': 'text/plain' });
+        response.end(`An error occurred: ${error.message}`);
+      } else {
+        response.writeHead(200, { 'Content-Type': 'image/png' });
+        response.end(content);
+      }
+    });
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.end('<h1>404 Nothing is Here</h1>');
+  }
 });
-  
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server running on port ${
